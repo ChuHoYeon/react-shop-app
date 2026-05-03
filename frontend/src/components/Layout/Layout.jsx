3b4273e4d759881f158f59e0eb8e02293926de36
ChuHoYeon <158002841+ChuHoYeon@users.noreply.github.com>
2026-05-03 19:26:12 +0900

    feat: implement initial site layout with responsive Header, Nav, and Footer components

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import styles from "./Layout.module.scss";
 
 const Layout = () => {
-    return <div>Layout</div>;
+    return (
+        <div className={styles.layout}>
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
+    );
 };
 
 export default Layout;
