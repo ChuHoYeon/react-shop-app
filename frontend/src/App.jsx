e38e00feb099d7138f9ab5a184ef387e660b9f03
ChuHoYeon <158002841+ChuHoYeon@users.noreply.github.com>
2026-05-03 19:39:21 +0900

    feat: add Login and Register pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Layout from "./components/layout/Layout";
+import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/Home/HomePage";
 import CartPage from "./pages/Cart/CartPage";
 import DetailPage from "./pages/Detail/DetailPage";
---
+++ frontend/src/pages/Register/SignUp/SignUp.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Form } from "react-router-dom";
+
+const SignUp = () => {
+    return <Form title={"가입하기"} />;
+};
+
+export default SignUp;
