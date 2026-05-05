9af795ae5bf5eb83fac16ba21b648460c0bc5b10
ChuHoYeon <158002841+ChuHoYeon@users.noreply.github.com>
2026-05-05 22:32:29 +0900

    Implement product listing and category filtering

---
+++ frontend/src/pages/Home/CardList/CardList.jsx
@@ -0,0 +1,28 @@
+import { useAppDispatch, useAppSelector } from "@/hooks/redux";
+import React, { useEffect } from "react";
+import { fetchProducts } from "@/store/products/products.slice";
+import styles from "./CardList.module.scss";
+import CardItem from "./CardItem/CardItem";
+import CardSkeleton from "../CardSkeleton/CardSkeleton";
+
+const CardList = () => {
+    const dispatch = useAppDispatch();
+    const { products, isLoading } = useAppSelector((state) => state.productsSlice);
+    const category = useAppSelector((state) => state.categoriesSlice);
+
+    useEffect(() => {
+        dispatch(fetchProducts(category?.toLowerCase()));
+    }, [category]);
+
+    if (isLoading) return <CardSkeleton />;
+
+    return (
+        <ul className={styles.card_list}>
+            {products.map((product) => (
+                <CardItem key={product.id} item={product} />
+            ))}
+        </ul>
+    );
+};
+
+export default CardList;
