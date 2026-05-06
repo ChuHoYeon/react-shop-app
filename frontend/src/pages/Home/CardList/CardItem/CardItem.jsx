b55f817809a8e3188b4be53fa48b61236a48a00a
ChuHoYeon <158002841+ChuHoYeon@users.noreply.github.com>
2026-05-07 00:55:17 +0900

    Feat: 장바구니 및 인증 네비게이션 개선
    장바구니 상품 개수를 네비게이션에 표시하고, 로그인 상태에 따라 로그인/로그아웃 버튼을 분기하도록 변경했다.
    상품 카드에서 장바구니에 담긴 제품을 다시 클릭하면 제거할 수 있도록 개선했다.

--- frontend/src/pages/Home/CardList/CardItem/CardItem.jsx
+++ frontend/src/pages/Home/CardList/CardItem/CardItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./CardItem.module.scss";
 import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "@/hooks/redux";
-import { addToCart } from "@/store/cart/cart.slice";
+import { addToCart, deleteFromCart } from "@/store/cart/cart.slice";
 
 const CardItem = ({ item }) => {
     const { products } = useAppSelector((state) => state.cartSlice);
@@ -11,6 +11,9 @@ const CardItem = ({ item }) => {
 
     const addItemToCart = () => {
         dispatch(addToCart(item));
+    };
+    const deleteProduct = () => {
+        dispatch(deleteFromCart(item.id));
     };
 
     return (
@@ -22,9 +25,16 @@ const CardItem = ({ item }) => {
             <h5>{item.title.substring(0, 15)}...</h5>
 
             <div>
-                <button disabled={productMatching} onClick={() => !productMatching && addItemToCart()}>
+                {!productMatching ? (
+                    <button onClick={addItemToCart}>장바구니에 담기</button>
+                ) : (
+                    <button className={styles.disabled} onClick={deleteProduct}>
+                        장바구니에 담긴 제품
+                    </button>
+                )}
+                {/* <button disabled={productMatching} onClick={() => !productMatching && addItemToCart()}>
                     {productMatching ? "장바구니에 담긴 제품" : "장바구니에 담기"}
-                </button>
+                </button> */}
                 <p>${item.price}</p>
             </div>
         </li>
