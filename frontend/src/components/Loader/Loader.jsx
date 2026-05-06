818b1b041f4813a9315c5662cd1d6d1c0585bfef
ChuHoYeon <158002841+ChuHoYeon@users.noreply.github.com>
2026-05-06 19:24:03 +0900

    feat: 로딩 인디케이터 컴포넌트 추가
    - 사용자에게 데이터 로딩 상태를 시각적으로 전달하기 위해 애니메이션이 포함된 범용 로더 컴포넌트를 구현합니다.

---
+++ frontend/src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "@/components/Loader/Loader.module.scss";
+
+const Loader = () => {
+    return (
+        <div className={style.lds_ring}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Loader;
