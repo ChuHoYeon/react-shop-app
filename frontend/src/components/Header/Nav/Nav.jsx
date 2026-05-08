1cc7140fc9f9e67f747d5f078e5d88aa92d9276e
ChuHoYeon <158002841+ChuHoYeon@users.noreply.github.com>
2026-05-08 19:32:05 +0900

    feat: 장바구니 hover 미리보기 구현
    네비게이션 장바구니 아이콘에서 상품 목록, 합계, 삭제 기능을 확인할 수 있는 미리보기 UI를 추가합니다.

--- frontend/src/components/Header/Nav/Nav.jsx
+++ frontend/src/components/Header/Nav/Nav.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { FiLogIn, FiShoppingCart, FiUser } from "react-icons/fi";
 import { GoSignOut } from "react-icons/go";
@@ -9,55 +8,65 @@ import app from "@/firebase";
 import { useAppDispatch, useAppSelector } from "@/hooks/redux";
 import { removeUser } from "@/store/user/user.slice";
 import { removeUserId } from "@/store/cart/cart.slice";
+import NavCartBlock from "./NavCartBlock/NavCartBlock";
 
 const Nav = () => {
-    const { isAuth } = useAuth();
-    const dispatch = useAppDispatch();
-    const auth = getAuth(app);
-    const { products } = useAppSelector((state) => state.cartSlice);
+  const { isAuth } = useAuth();
+  const dispatch = useAppDispatch();
+  const auth = getAuth(app);
+  const { products } = useAppSelector((state) => state.cartSlice);
 
-    const handleSignout = () => {
-        signOut(auth)
-            .then(() => {
-                dispatch(removeUser());
-                dispatch(removeUserId());
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    };
-    return (
-        <nav className={styles.nav}>
-            <ul>
-                <li>
-                    <div className={styles.counter}>
-                        <Link to={"/cart"}>
-                            {" "}
-                            <FiShoppingCart />
-                        </Link>
-                        {products.length > 0 && <b>{products.length}</b>}
-                    </div>
-                </li>
-                <li>
-                    <div className={styles.counter}>
-                        <Link to={"/order"}>
-                            {" "}
-                            <FiUser title="주문" />
-                        </Link>
-                    </div>
-                </li>
-                <li>
-                    {isAuth ? (
-                        <GoSignOut className={styles.nav_sign_out} title="로그아웃" onClick={handleSignout} />
-                    ) : (
-                        <Link to={"/login"}>
-                            <FiLogIn title="로그인" />
-                        </Link>
-                    )}
-                </li>
-            </ul>
-        </nav>
-    );
+  const handleSignout = () => {
+    signOut(auth)
+      .then(() => {
+        dispatch(removeUser());
+        dispatch(removeUserId());
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <div className={styles.counter}>
+            <Link to={"/cart"}>
+              {" "}
+              <FiShoppingCart />
+            </Link>
+            {products.length > 0 && <b>{products.length}</b>}
+            {products.length > 0 && (
+              <div className={styles.nav_hover_cart}>
+                <NavCartBlock />
+              </div>
+            )}
+          </div>
+        </li>
+        <li>
+          <div className={styles.counter}>
+            <Link to={"/order"}>
+              {" "}
+              <FiUser title="주문" />
+            </Link>
+          </div>
+        </li>
+        <li>
+          {isAuth ? (
+            <GoSignOut
+              className={styles.nav_sign_out}
+              title="로그아웃"
+              onClick={handleSignout}
+            />
+          ) : (
+            <Link to={"/login"}>
+              <FiLogIn title="로그인" />
+            </Link>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
